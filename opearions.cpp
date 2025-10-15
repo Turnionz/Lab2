c485bff85fd3a57827edd4506b7a54b8404a614a
Turnionz <turn0nz@gmail.com>
2025-10-15 19:57:27 +0300

    Initial cpp files

---
+++ opearions.cpp
@@ -0,0 +1,18 @@
+#include operations.h
+
+float add(float a, float b) {
+	return a + b;
+};
+
+flaot subtract(float, float b) {
+	return a - b;
+};
+
+float mult(float a, float b) {
+	return a * b;
+};
+
+
+float subdiv(float a, float b) {
+	return a / b;
+};
---
+++ operations.h
@@ -0,0 +1,9 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+float add(float a, float b);
+float subtract(float a, float b);
+float mult(float a, float b);
+float subdiv(float a, float b);
+
+#endif
